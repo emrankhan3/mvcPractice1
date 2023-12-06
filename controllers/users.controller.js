9459b9667ee8b7b2c7f3f4d87c2e8d14a19cae4b
emran <emranbspt@gmail.com>
2023-12-06 18:32:53 +0600

    add models views controllers routes folders and files

---
+++ controllers/users.controller.js
@@ -0,0 +1,14 @@
+exports.getUsers = (req,res) => {
+    res.send('bismillah')
+};
+exports.saveUser = (req, res)=>{
+    const name = req.body.name
+    const age = Number(req.body.age)
+    const user = {
+        name,
+        age
+    }
+    users.push(user)
+    res.status(200).send('paisimona')
+
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+const userRouter = require('./routes/users.route')
+app.use(express.urlencoded({ extended: true }));
+app.use(userRouter)
+
+const htmlForm =`
+    <form action='/' method='post'>
+        <input type='text' name='name' placeholder='Name' />
+        <input type='number' name='age' placeholder='age' />
+        <button type='submit' name='btn'>save</button>
+    </form>
+`
+let users = [
+    { name: 'John', age:42},
+    { name: 'khaled', age:94}
+]
+
+
+
+
+app.use((req,res,next)=>{
+    res.status(404).json({message:"resource not found!"})
+})
+
+
+app.listen(3001,()=>{
+    console.log('server running on localhost:3001');
+})
